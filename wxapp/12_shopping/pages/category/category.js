9ae1069bfb2333bee6b9a7e776e627588ac90065
shushiqi <1539588925@qq.com>
2019-05-13 14:23:05 +0800

    shoppingMall

---
+++ wxapp/12_shopping/pages/category/category.js
@@ -0,0 +1,124 @@
+// pages/category/category.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    category: [{
+        name: '果味',
+        id: 'guowei'
+      },
+      {
+        name: '蔬菜',
+        id: 'shucai'
+      },
+      {
+        name: '炒货',
+        id: 'chaohuo'
+      },
+      {
+        name: '点心',
+        id: 'dianxin'
+      },
+      {
+        name: '粗茶',
+        id: 'cucha'
+      },
+      {
+        name: '淡饭',
+        id: 'danfan'
+      }
+    ],
+    detail: [],
+    curIndex: 0,
+    toView: 'guowei',
+    isScroll: false
+  },
+  swtichTab(e){
+    // console.log(e);
+    
+    this.setData({
+      curIndex: e.target.dataset.index,
+      toView: e.target.dataset.id
+    })
+  },
+  scrollHandle(e){
+    console.log(e);
+ 
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    let self = this
+    wx.request({
+      url: 'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        console.log(res.data);
+        
+        self.setData({
+          detail: res.data
+        })
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
