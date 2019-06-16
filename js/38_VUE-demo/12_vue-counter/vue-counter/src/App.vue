cad952dc06836474bd239602c0fc802a5c36b835
shushiqi <1539588925@qq.com>
2019-06-16 10:04:39 +0800

    vuex 思想

---
+++ js/38_VUE-demo/12_vue-counter/vue-counter/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Counter />
+    <br>
+    <Counter-brother />
+  </div>
+</template>
+
+<script>
+import Counter from "./components/counter";
+import CounterBrother from "./components/counterBrother";
+
+export default {
+  name: "App",
+  data() {
+    return {
+    };
+  },
+  methods: {
+    
+  },
+  components: {
+    Counter: Counter,
+    CounterBrother: CounterBrother
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ js/38_VUE-demo/12_vue-counter/vue-counter/src/components/counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{$store.state.count}} times,
+    <button @click="increment">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    
+  }
+};
+</script>
+
+<style>
+</style>
