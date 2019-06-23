dfdfd2e65b49e94a60b5d9a6769440af4bc4e477
shushiqi <1539588925@qq.com>
2019-06-23 12:40:30 +0800

    react-ANT

--- js/40_react/02_ant/react-router-ant/src/App.js
+++ js/40_react/02_ant/react-router-ant/src/App.js
@@ -1,74 +1,19 @@
 import React from 'react';
-import {
-  Layout, Row, Col, Avatar, Input,
-  Menu, Dropdown, Icon, Badge
-} from 'antd';
-import { Link } from 'react-router-dom';
+import {BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import Layout from './page/Layout';
+import logo from './logo.svg';
+import 'antd/dist/antd.css';
+// import './App.css';
 
-const { Header, Footer, Sider, Content } = Layout;
-function DropMenu() {
+function App() {
   return (
-    <Menu>
-      <Menu.Item>
-        <a href="">1</a>
-      </Menu.Item>
-      <Menu.Item>
-        <a href="">2</a>
-      </Menu.Item>
-    </Menu>
-  )
+    <Router>
+      <Route path="/" component={Layout}></Route>
+    </Router>
+  );
 }
-class PageLayout extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <Header style={{ color: '#fff', textAlign: 'center', fontWeight: 'bold' }}>
-          <Row>
-            <Col span={10}>
-              React + Antd 实践
-            </Col>
-            <Col span={8}>
-              <Input placeholder="请输入你想要的......"></Input>
-            </Col>
-            <Col span={6}>
-              <Avatar style={{ backgroundColor: '#666', marginRight: 20 }}
-                icon="user"
-              ></Avatar>
-              <Dropdown overlay={DropMenu}>
-                <span>Hi,
-                  <Icon type="down"></Icon>
-                </span>
-              </Dropdown>
-            </Col>
-          </Row>
-        </Header>
-        <Layout>
-          <Sider>
-            <Menu
-              style={{ width: 256, height: '90vh', overflow: 'auto', minWidth: 256 }}
-              defaultOpenKeys={['sub1']}
-              mode="inline"
-            >
-              <Menu.SubMenu key="sub1" title={
-                <span><Icon type="smile-0"></Icon>部分ui组件实战</span>
-              }>
-                <Menu.Item>
-                  {/* /table */}
-                  <Link to={"table"}>表格</Link>
-                </Menu.Item>
 
-              </Menu.SubMenu>
-            </Menu>
-          </Sider>
-          <Content>
-            {this.props.children}
-          </Content>
-        </Layout>
-        <Footer>
-          Footer
-        </Footer>
-      </Layout>
-    )
-  }
-}
-export default PageLayout;
+export default App;
