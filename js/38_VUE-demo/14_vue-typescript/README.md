ebfb25b1d74275807f6c036c334ad9aef2211411
shushiqi <1539588925@qq.com>
2019-07-09 10:22:17 +0800

    vue-typescript

---
+++ js/38_VUE-demo/14_vue-typescript/README.md
@@ -0,0 +1,12 @@
+## ts的类型约束
+  - 类型约束 => 报错
+  - 代码提示 => 更加友好
+
+
+***
+- vue-cli 3.0+ 原生命令行支持typescript   
+  react 最早拥抱typescript vue加入
+- 项目多加20% typescript代码 减少60%的潜在bug  =>  源于js的弱类型   
+  良好的代码提示，让团队合作体验更好
+- 重要的对象（组件、state、prop、vuex/redux、函数） 需要声明类型    
+  vuex最大的对象 => state
---
+++ js/38_VUE-demo/14_vue-typescript/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">hashimoto kanna</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { Action } from "vuex-class";
+
+@Component
+export default class Home extends Vue {
+  private cate: string = "in_theaters";
+  private active: number = 0;
+  private loading: boolean = true;
+  private movieListData = null;
+  @Action private movieList!: (cate: string) => any;
+
+  private test() {
+    console.log("----");
+  }
+
+  private created() {
+    this.movieList(this.cate).then((res: any) => {
+      this.movieListData = res.subjects;
+      this.loading = false;
+      console.log(res.subjects);
+    });
+  }
+}
+</script>
